9b1603543b149eb81594b73eb12b5985a59cd6bc
Kaelinator <kirkkael@gmail.com>
2025-05-06 03:46:27 -0500

    added CollapsibleSection

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,34 +1,50 @@
 <template>
-  <div class="wrapper">
-    <div class="bar">
+  <div :class="$style.wrapper">
+    <div :class="$style.bar">
+      <CollapsibleSection title="Project Settings">
+        <li v-for="index in 10" :key="index">
+          {{ index }}
+        </li>
+      </CollapsibleSection>
+      <CollapsibleSection title="Data Points">
+        <li v-for="index in 50" :key="index">
+          {{ index }}
+        </li>
+      </CollapsibleSection>
     </div>
 
-    <div class="content">
+    <div :class="$style.content">
     </div>
 
-    <div class="bar">
+    <div :class="$style.bar">
+      <CollapsibleSection title="Component List">
+        <li v-for="index in 10" :key="index">
+          {{ index }}
+        </li>
+      </CollapsibleSection>
     </div>
   </div>
 </template>
 
 <script setup>
+  import CollapsibleSection from './components/CollapsibleSection.vue';
 </script>
 
-<style>
-.wrapper {
-  display: grid;
-  grid-template-columns: 300px auto 300px;
-  height: 100vh;
-}
+<style module>
+  .wrapper {
+    display: grid;
+    grid-template-columns: 300px auto 300px;
+    height: 100vh;
+  }
 
-.bar {
-  background: #d9d9d9;
-  display: flex;
-  flex-flow: column nowrap;
-  overflow-y: scroll;
-}
+  .bar {
+    background: #d9d9d9;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
 
-.content {
-  background: #E2E2E2;
-}
+  .content {
+    background: #E2E2E2;
+    height: 100vh;
+  }
 </style>
